Fix bug on GPU Hessian calculation
Committer: ericyuan00000 <ericyuan@n0000.scs00>

On branch main
Your branch is ahead of 'origin/main' by 1 commit.
  (use "git push" to publish your local commits)

Changes to be committed:
  (use "git reset HEAD^1 <file>..." to unstage)

modified:   cli/newtonnet_train
modified:   newtonnet/models/newtonnet.py

newtonnet/models/newtonnet.py
@@ -239,17 +239,18 @@ class NewtonNet(nn.Module):
                 create_graph=self.create_graph,
                 retain_graph=True
             )[0]
-
-            ddE = torch.zeros(E.shape[0], R.shape[1], R.shape[2], R.shape[1], R.shape[2])
-            for A_ in range(R.shape[1]):
-                for X_ in range(3):
-                    ddE[:, A_, X_, :, :] = grad(
-                        dE[:, A_, X_],
-                        R,
-                        grad_outputs=torch.ones(E.shape[0]),
-                        create_graph=self.create_graph,
-                        retain_graph=True
-                    )[0]
+            
+            if self.return_intermediate:
+                ddE = torch.zeros(E.shape[0], R.shape[1], R.shape[2], R.shape[1], R.shape[2], device=R.device)
+                for A_ in range(R.shape[1]):
+                    for X_ in range(3):
+                        ddE[:, A_, X_, :, :] = grad(
+                            dE[:, A_, X_],
+                            R,
+                            grad_outputs=torch.ones(E.shape[0], device=R.device),
+                            create_graph=self.create_graph,
+                            retain_graph=True
+                        )[0]
 
             dE = -1.0 * dE
 
@@ -259,7 +260,7 @@ class NewtonNet(nn.Module):
         if self.return_intermediate:
             return {'R': R, 'E': E, 'F': dE, 'H': ddE, 'Ei': Ei, 'hs': hs, 'F_latent': f_dir}
         else:
-            return {'R': R, 'E': E, 'F': dE, 'H': ddE, 'Ei': Ei, 'F_latent': f_dir}
+            return {'R': R, 'E': E, 'F': dE, 'Ei': Ei, 'F_latent': f_dir}
 
 
 class DynamicsCalculator(nn.Module):