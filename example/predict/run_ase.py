fcf0e85c887c4367d73a4122d59ad770bc1e31dc
ericyuan <ericyuan@berkeley.edu>
2023-05-09 11:33:10 -0700

    Update hessian vectorization

--- example/predict/run_ase.py
+++ example/predict/run_ase.py
@@ -4,7 +4,9 @@ from ase import Atoms
 
 mlcalculator = MLAseCalculator(model_path='training_18/models/best_model_state.tar',
                                settings_path='training_18/run_scripts/config0.yml',
-                               device='cpu')
+                               mode='autograd', 
+                               diff_precision=0.0,
+                               device=['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3'])
 
 h2 = Atoms('H2',
            positions=[[0.0, 0.0, 0.0],
