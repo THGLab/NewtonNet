87ee3e461babdf830c384994d6ce99dfb08a8b9e
Eric Yuan <87563575+ericyuan00000@users.noreply.github.com>
2023-05-16 16:51:10 -0700

    Update ASE options

--- example/predict/run_ase.py
+++ example/predict/run_ase.py
@@ -4,7 +4,7 @@ from ase import Atoms
 
 mlcalculator = MLAseCalculator(model_path='training_18/models/best_model_state.tar',
                                settings_path='training_18/run_scripts/config0.yml',
-                               method='fwd_diff',
+                               method='fwd_diff',    # 'autograd', 'fwd_diff', or None
                                grad_precision=0.0001,    # optional, grid size in Angstrom for Hessian gradient calculation (default: 0.0001)
                                device='cpu'   # 'cpu' or list of cuda
                )
