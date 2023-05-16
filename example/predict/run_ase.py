cbee30e28fc5b2666e2b22e488dff8716f57090c
Eric Yuan <87563575+ericyuan00000@users.noreply.github.com>
2023-05-16 16:50:10 -0700

    Update ASE options

--- example/predict/run_ase.py
+++ example/predict/run_ase.py
@@ -4,8 +4,9 @@ from ase import Atoms
 
 mlcalculator = MLAseCalculator(model_path='training_18/models/best_model_state.tar',
                                settings_path='training_18/run_scripts/config0.yml',
-                               grad_precision=0.0,    # optional, grid size in Angstrom for Hessian gradient calculation (default: 0.0, i.e. automatic differentiation)
-                               device=['cuda:0', 'cuda:1', 'cuda:2', 'cuda:3']    # 'cpu' or list of cuda
+                               method='fwd_diff',
+                               grad_precision=0.0001,    # optional, grid size in Angstrom for Hessian gradient calculation (default: 0.0001)
+                               device='cpu'   # 'cpu' or list of cuda
                )
 
 h2 = Atoms('H2',
