6e2e0cde7daca9eb9477b3a0a9570c8038abf5eb
ericyuan <ericyuan@berkeley.edu>
2023-06-16 20:43:46 -0700

    Update outlier test

--- example/predict/run_ase.py
+++ example/predict/run_ase.py
@@ -11,14 +11,17 @@ mlcalculator = MLAseCalculator(
                    'training_9/run_scripts/config.yml',
                    'training_10/run_scripts/config.yml',
                    'training_11/run_scripts/config.yml'],
-    method='autograd',    # 'autograd', 'fwd_diff', or None
-    #grad_precision=0.0001,    # optional, grid size in Angstrom for Hessian gradient calculation (default: 0.0001)
+    method='autograd',    # 'autograd', 'fwd_diff', 'cnt_diff', or None (default: 'autograd')
+    # grad_precision=1e-5,    # optional, grid size in Angstrom for Hessian gradient calculation (default: None)
     device='cpu'   # 'cpu' or list of cuda
 )
 
-h2 = Atoms('H2',
-           positions=[[0.0, 0.0, 0.0],
-                      [0.0, 0.0, 1.0]])
+h2 = Atoms(numbers=[1, 1], positions=[[0.0, 0.0, 0.0], [0.0, 0.0, 1.0]])
 mlcalculator.calculate(h2)
 
-print(mlcalculator.results)
+print(mlcalculator.results['energy'])
+print(mlcalculator.results['forces'])
+print(mlcalculator.results['hessian'])
+print(mlcalculator.results['energy_std'])
+print(mlcalculator.results['forces_std'])
+print(mlcalculator.results['hessian_std'])
