8937014ad309881dd8fa3435a596d4de6071a9e7
ericyuan <ericyuan@berkeley.edu>
2023-07-13 14:08:50 -0700

    Update trained models

--- example/predict/run_ase.py
+++ example/predict/run_ase.py
@@ -3,16 +3,17 @@ from newtonnet.utils.ase_interface import MLAseCalculator
 from ase import Atoms
 
 mlcalculator = MLAseCalculator(
-    model_path=['training_8/models/best_model_state.tar',
-                'training_9/models/best_model_state.tar',
-                'training_10/models/best_model_state.tar',
-                'training_11/models/best_model_state.tar'],    # path to model file, str or list of str
-    settings_path=['training_8/run_scripts/config.yml',
-                   'training_9/run_scripts/config.yml',
-                   'training_10/run_scripts/config.yml',
-                   'training_11/run_scripts/config.yml'],    # path to configuration file, str or list of str
-    method='autograd',    # 'autograd', 'fwd_diff', 'cnt_diff', or None (default: 'autograd')
-    # grad_precision=1e-5,    # optional, grid size in Angstrom for Hessian gradient calculation (default: None)
+    model_path=['training_52/models/best_model_state.tar',
+                'training_53/models/best_model_state.tar',
+                'training_54/models/best_model_state.tar',
+                'training_55/models/best_model_state.tar'],    # path to model file, str or list of str
+    settings_path=['training_52/run_scripts/config0.yml',
+                   'training_53/run_scripts/config2.yml',
+                   'training_54/run_scripts/config1.yml',
+                   'training_55/run_scripts/config3.yml'],    # path to configuration file, str or list of str
+    method='autograd',    # method to calculate hessians. 'autograd', 'fwd_diff', 'cnt_diff', or None (default: 'autograd')
+    # grad_precision=1e-5,    # hessian gradient calculation precision for 'fwd_diff' and 'cnt_diff', ignored otherwise (default: None)
+    disagreement='std',    # method to calculate disagreement among models. 'std', 'std_outlierremoval', 'range':, 'values', or None (default: 'std')
     device='cpu'   # 'cpu' or list of cuda
 )
 
@@ -22,7 +23,7 @@ mlcalculator.calculate(h2)
 print(mlcalculator.results['energy'])    # mean of calculated molecular energies, shape (1,)
 print(mlcalculator.results['forces'])    # mean of calculated atomic forces, shape (n_atom, 3)
 print(mlcalculator.results['hessian'])    # mean of calculated atomic Hessian, shape (n_atom, 3, n_atom, 3)
-print(mlcalculator.results['energy_std'])    # standard deviation of calculated molecular energies, shape (1,)
-print(mlcalculator.results['forces_std'])    # standard deviation of calculated atomic forces, shape (n_atom, 3)
-print(mlcalculator.results['hessian_std'])    # standard deviation of calculated atomic Hessian, shape (n_atom, 3, n_atom, 3)
+print(mlcalculator.results['energy_disagreement'])    # disagreement among calculated molecular energies, shape (1,)
+print(mlcalculator.results['forces_disagreement'])    # disagreement among calculated atomic forces, shape (n_atom, 3)
+print(mlcalculator.results['hessian_disagreement'])    # disagreement among calculated atomic Hessians, shape (n_atom, 3, n_atom, 3)
 print(mlcalculator.results['outlier'])    # index of model that predicts an energy outlier, int
