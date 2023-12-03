fe09a2849af53b59a82e596c6e0afc82cc33fe6b
ericyuan <ericyuan@berkeley.edu>
2023-12-02 16:27:01 -0800

    Update flexible data properties

--- newtonnet/data/loader.py
+++ newtonnet/data/loader.py
@@ -11,26 +11,29 @@ class MolecularDataset(Dataset):
         The dictionary of batch data in ndarray format.
 
     """
-    def __init__(self, input, environment=NeighborEnvironment(), device: torch.device = torch.device('cpu')):
+    def __init__(self, input, properties=['E', 'F'], environment=NeighborEnvironment(), device: torch.device = torch.device('cpu')):
         self.R = torch.tensor(input['R'], dtype=torch.float)
         self.Z = torch.tensor(input['Z'], dtype=torch.long)
-        self.E = torch.tensor(input['E'], dtype=torch.float)
-        self.F = torch.tensor(input['F'], dtype=torch.float)
         self.N, self.NM, self.AM, self.D, self.V = environment.get_environment(self.R, self.Z)
+        self.properties = properties
+        for property in self.properties:
+            if property in input.keys():
+                setattr(self, property, torch.tensor(input[property], dtype=torch.float))
+            else:
+                raise ValueError(f'property {property} is not in the input data')
         self.device = device
+        # self.normalizer = None
 
     def __getitem__(self, index):
         output = dict()
-        output['R'] = self.R[index].to(self.device)
-        output['Z'] = self.Z[index].to(self.device)
-        output['E'] = self.E[index].to(self.device)
-        output['F'] = self.F[index].to(self.device)
-        output['AM'] = self.AM[index].to(self.device)
-        output['N'] = self.N[index].to(self.device)
-        output['NM'] = self.NM[index].to(self.device)
-        output['D'] = self.D[index].to(self.device)
-        output['V'] = self.V[index].to(self.device)
+        for property in ['R', 'Z', 'AM', 'N', 'NM', 'D', 'V'] + self.properties:
+            output[property] = getattr(self, property)[index].to(self.device)
         return output
 
     def __len__(self):
         return self.Z.shape[0]
+    
+    # def set_normalizer(self, normalizer):
+    #     self.normalizer = normalizer
+    #     self.E = self.normalizer.forward(self.E, self.Z, scale=True, shift=True)
+    #     self.F = self.normalizer.forward(self.F, self.Z, scale=True, shift=False)
