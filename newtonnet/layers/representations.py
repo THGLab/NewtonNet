4158fa26adae030c2e79d2d62ab5629fc3fc4ef0
ericyuan <ericyuan@berkeley.edu>
2023-12-14 12:35:52 -0800

    Update representation function comment

--- newtonnet/layers/representations.py
+++ newtonnet/layers/representations.py
@@ -1,37 +1,38 @@
-import numpy as np
-
 import torch
 from torch import nn
 
 
 class RadialBesselLayer(nn.Module):
-    r"""Radial Bessel functions based on the work by DimeNet: https://github.com/klicperajo/dimenet
+    '''
+    Radial Bessel functions based on the work by DimeNet: https://github.com/klicperajo/dimenet
 
-    Args:
-        n_radials (int, optional): total number of radial functions, :math:`N_g`.
-        cutoff (float, optional): cutoff, :math:`\mu_{r_c}`
-
-    """
+    Parameters:
+        n_radials (int): Total number of radial functions. Default: 16.
+        cutoff (float): Cutoff. Default: 5.0.
+        device (torch.device): Device. Default: torch.device('cpu').
+    '''
 
     def __init__(
-        self, n_radial=16, cutoff=5.0, device=None
+        self, 
+        n_basis: int = 16,
+        cutoff: float = 5.0,
+        device: torch.device = torch.device('cpu'),
     ):
         super(RadialBesselLayer, self).__init__()
         self.inv_cutoff = 1 / cutoff
-        self.frequencies = nn.Parameter(torch.arange(1, n_radial + 1, requires_grad=False, device=device) * torch.pi)
+        self.frequencies = nn.Parameter(torch.arange(1, n_basis + 1, requires_grad=False, device=device) * torch.pi)
         self.epsilon = 1.0e-8
 
     def forward(self, distances):
-        """Compute smeared-gaussian distance values.
+        '''
+        Compute smeared-gaussian distance values.
 
-        Args:
-            distances (torch.Tensor): interatomic distance values of
-                (N_b x N_at x N_nbh) shape.
+        Arguments:
+            distances (torch.Tensor): interatomic distance values of shape (batch_size, n_atoms, n_atoms).
 
         Returns:
-            torch.Tensor: layer output of (N_b x N_at x N_nbh x N_g) shape.
-
-        """
+            torch.Tensor: edge embedding of shape (batch_size, n_atoms, n_atoms, n_radials).
+        '''
         d_scaled = distances * self.inv_cutoff
         d_scaled = d_scaled.unsqueeze(-1)
         out = torch.sin(self.frequencies * d_scaled) / (d_scaled + self.epsilon) / self.frequencies
