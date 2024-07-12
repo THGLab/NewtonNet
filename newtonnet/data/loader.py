d60d1192119cda82b2b6cd0f076bc002849286b3
Eric Yuan <ericyuan@berkeley.edu>
2024-07-11 17:44:24 -0700

    Update PyG Dataset

--- newtonnet/data/loader.py
+++ newtonnet/data/loader.py
@@ -1,70 +1,66 @@
+import os
+import os.path as osp
+from typing import Callable, List, Optional, Union
+
+import numpy as np
 import torch
-from torch import nn
-from torch.utils.data import Dataset
+from torch_geometric.data import InMemoryDataset, Data
 
-# from newtonnet.data import NeighborEnvironment
-from newtonnet.layers.scalers import Normalizer
+from newtonnet.data.neighbors import RadiusGraph
 
 
-class MolecularDataset(Dataset):
-    '''
-    This class is a dataset for molecular data.
+class MolecularDataset(InMemoryDataset):
+    def __init__(
+        self,
+        root: str,
+        transform: Optional[Callable] = None,
+        pre_transform: Optional[Callable] = None,
+        pre_filter: Optional[Callable] = None,
+        force_reload: bool = False,
+    ) -> None:
 
-    Parameters:
-        input (dict): The input data.
-        properties (list): The properties to be predicted. Default: ['E', 'F'].
-        environment (NeighborEnvironment): The environment for the dataset. Default: NeighborEnvironment().
-        device (torch.device): The device for the dataset. Default: torch.device('cpu').
-    '''
-    def __init__(self, input, properties=['energy', 'forces'], environment=NeighborEnvironment(), device=torch.device('cpu'), precision=torch.float32):
-        self.properties = properties
-        self._check_properties(input)
-        self._check_shapes(input)
-        self.positions = torch.tensor(input['positions'], dtype=precision)
-        self.atomic_numbers = torch.tensor(input['atomic_numbers'], dtype=torch.int)
-        self.distances, self.distance_vectors, self.atom_mask, self.neighbor_mask = environment.get_environment(self.positions, self.atomic_numbers)
-        for key in self.properties:
-            setattr(self, key, torch.tensor(input[key], dtype=precision))
-        self.device = device
-        self.normalized = False
+        super().__init__(root, transform, pre_transform, pre_filter,
+                         force_reload=force_reload)
 
-    def _check_properties(self, input):
-        for key in ['positions', 'atomic_numbers'] + self.properties:
-            assert key in input.keys(), f'property {key} is not in the input data'
+        self.load(self.processed_paths[0])
 
-    def _check_shapes(self, input):
-        assert input['positions'].ndim == 3, 'positions must have 3 dimensions (n_data, n_atoms, 3).'
-        assert input['atomic_numbers'].ndim == 2, 'atomic_numbers must have 2 dimensions (n_data, n_atoms).'
-        assert input['positions'].shape[0] == input['atomic_numbers'].shape[0], 'positions and atomic_numbers must have same dimension 0 (n_data).'
-        assert input['positions'].shape[1] == input['atomic_numbers'].shape[1], 'positions and atomic_numbers must have same dimension 1 (n_atoms).'
-        assert input['positions'].shape[2] == 3, 'positions must have 3 coordinates at dimension 2 (x, y, z).'
-        for key in self.properties:
-            assert input[key].shape[0] == input['positions'].shape[0], f'{key} and positions must have same dimension 0 (n_data).'
+    @property
+    def raw_dir(self) -> str:
+        return osp.join(self.root, 'raw')
 
-    def __getitem__(self, index):
-        output = dict()
-        for key in ['positions', 'atomic_numbers', 'distances', 'distance_vectors', 'atom_mask', 'neighbor_mask']:
-            output[key] = getattr(self, key)[index].to(self.device)
-        for key in self.properties:
-            output[key] = getattr(self, key)[index].to(self.device)
-        if self.normalized:
-            for key in self.properties:
-                output[key + '_normalized'] = getattr(self, key + '_normalized')[index].to(self.device)
-        return output
+    @property
+    def processed_dir(self) -> str:
+        return osp.join(self.root, 'processed')
 
-    def __len__(self):
-        return self.atomic_numbers.shape[0]
-    
-    def get(self, key):
-        return getattr(self, key)
-    
-    def normalize(self, normalizers: nn.ModuleDict):
-        '''
-        Normalize the properties.
+    @property
+    def raw_file_names(self) -> Union[str, List[str]]:
+        names = [name for name in os.listdir(self.raw_dir) if name.endswith('.npz')]
+        return names
 
-        Args:
-            normalizers (nn.ModuleDict): The normalizers for each property.
-        '''
-        for key, normalizer in normalizers.items():
-            setattr(self, key + '_normalized', normalizer.forward(getattr(self, key), self.atomic_numbers))
-        self.normalized = True
+    @property
+    def processed_file_names(self) -> List[str]:
+        return ['data.pt']
+
+    def process(self) -> None:
+        data_list = []
+        processed_path = self.processed_paths[0]
+        for raw_path in self.raw_paths:
+            raw_data = np.load(raw_path)
+
+            z = torch.from_numpy(raw_data['Z']).int()
+            pos = torch.from_numpy(raw_data['R']).float()
+            energy = torch.from_numpy(raw_data['E']).float()
+            force = torch.from_numpy(raw_data['F']).float()
+
+            for i in range(pos.size(0)):
+                data = Data(z=z, pos=pos[i], energy=energy[i], force=force[i])
+                if self.pre_filter is not None and not self.pre_filter(data):
+                    continue
+                if self.pre_transform is not None:
+                    data = self.pre_transform(data)
+                data_list.append(data)
+
+        self.save(data_list, processed_path)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({len(self)}')"
