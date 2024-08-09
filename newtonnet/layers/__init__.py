fc1edf0263726bdd5817a431dc4dc17b2f087ed2
ericyuan00000 <ericyuan@berkeley.edu>
2024-08-09 15:12:34 -0700

    Revise output

--- newtonnet/layers/__init__.py
+++ newtonnet/layers/__init__.py
@@ -1,6 +1,4 @@
 from newtonnet.layers.activations import *
-from newtonnet.layers.cutoff import *
 from newtonnet.layers.precision import *
 from newtonnet.layers.representations import *
 from newtonnet.layers.scalers import *
-from newtonnet.layers.shells import *
